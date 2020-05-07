3062ab12ea04c8f5acd925e586a918883fcc8440
Andrew Warner <warneracw@gmail.com>
2020-05-07 14:36:08 -0400

    updted URL

---
+++ charter-app/src/constants.js
@@ -0,0 +1,12 @@
+// Declare Constants
+
+/////////////////////////////////////////////////
+// API URL and Key
+/////////////////////////////////////////////////
+const URL = 'https://code-challenge.spectrumtoolbox.com/api/restaurants';
+const APIKEY = 'q3MNxtfep8Gt';
+
+export {
+	URL,
+	APIKEY
+}
